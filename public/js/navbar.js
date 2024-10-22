149fdabf389c63de2d20c430e49a2d64fbfe37b0
Vítor Assunção <vitor.assuncaosantos100@gmail.com>
2024-10-21 23:28:58 -0300

    Refina alguns pontos da navbar como a adição de um hover e a mudança da fonte com o tamanho da tela. Todavia, ainda, está sem responsividade

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,2 +1,10 @@
+/* To-do
+1 - Definir rapidamente a animação do input do buscar(Deve criar um input no lado direito do ícone de loop -- este será um botão)
+*/
+
 /* Animação da abetura do buscar */
+const $liBuscar = document.querySelector("#navbar").lastElementChild.children[1];
+$liBuscar.addEventListener('click', ()=>{
+    /* Sendo feito */
+});
 

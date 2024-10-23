9f313c6e184038f36d15041d1fbc3d6e66ef2da4
Vítor Assunção <vitor.assuncaosantos100@gmail.com>
2024-10-23 02:48:12 -0300

    Adiciona a responsividade do navbar e uma animação para o botão de buscar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,10 +1,27 @@
-/* To-do
-1 - Definir rapidamente a animação do input do buscar(Deve criar um input no lado direito do ícone de loop -- este será um botão)
-*/
-
 /* Animação da abetura do buscar */
-const $liBuscar = document.querySelector("#navbar").lastElementChild.children[1];
-$liBuscar.addEventListener('click', ()=>{
-    /* Sendo feito */
-});
+const $liBuscar = document.querySelector("#navbar").children[1].children[1];
+const $inputSearchForm = $liBuscar.firstElementChild.lastElementChild;
+const $pBuscar = $liBuscar.lastElementChild;
 
+$liBuscar.addEventListener('mouseover', e =>{
+    $inputSearchForm.style.width = "105px";
+    $inputSearchForm.style.padding = "0 0 0 1.5vw";
+    $pBuscar.style.opacity = "0";
+    $pBuscar.style.width = "0";
+});
+$liBuscar.addEventListener('mouseout', e =>{
+    $inputSearchForm.style.width = "0";
+    $inputSearchForm.style.padding = "0 0 0 0";
+    $pBuscar.style.opacity = "100";
+    $pBuscar.style.width = "auto"
+});
+/* Animação ao clicar no ícone de menu */
+const abrirATableAndMobileSubSection = (menuIcon)=>{
+    menuIcon.classList.toggle("muda");
+    const subSection = document.querySelector("#tabletAndMobileSubSection");
+    if(menuIcon.classList[0] == "muda"){
+        subSection.style.display = "flex";
+    }else{
+        subSection.style.display = "none";
+    }
+}

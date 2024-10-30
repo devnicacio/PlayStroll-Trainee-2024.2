8ebbcd6f47b9f9bfaee00cca2c1728728aa03388
Vítor Assunção <vitor.assuncaosantos100@gmail.com>
2024-10-30 12:02:09 -0300

    Altera espaçamentos no navbar e melhora o tamanho da barra de buscar da mesma.

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -3,13 +3,13 @@ const $liBuscar = document.querySelector("#navbar").children[1].children[1];
 const $inputSearchForm = $liBuscar.firstElementChild.lastElementChild;
 const $pBuscar = $liBuscar.lastElementChild;
 
-/*$liBuscar.addEventListener('mouseover', e =>{
+/*$liBuscar.addEventListener('click', e =>{
     $inputSearchForm.style.width = "105px";
     $inputSearchForm.style.padding = "0 0 0 1.5vw";
     $pBuscar.style.opacity = "0";
     $pBuscar.style.width = "0";
 });
-$liBuscar.addEventListener('mouseout', e =>{
+$document.addEventListener('click', e =>{
     $inputSearchForm.style.width = "0";
     $inputSearchForm.style.padding = "0 0 0 0";
     $pBuscar.style.opacity = "100";

de1cd7aba7da0a16d36fd97863a6d30acec098fe
Diego Pereira Betti <diegopbetti.gmail.com>
2024-10-30 21:49:58 -0300

    Cria a div tela que vai dar opacidade quando abrir o modal. Cria o CSS e o JS para ter opacidade

--- public/js/excluir.js
+++ public/js/excluir.js
@@ -1,7 +1,11 @@
+const tela = document.querySelector('#tela');
+
 function abrirModal(idModal) {
     document.getElementById(idModal).style.display = "flex";
+    tela.style.display = 'block';
 }
 
 function fecharModal(idModal){
     document.getElementById(idModal).style.display = "none";
+    tela.style.display = 'none';
 }

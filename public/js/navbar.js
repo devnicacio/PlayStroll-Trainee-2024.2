30e099037849d9667ebe95007c286b618fec123a
Vitor Assuncao <vitor.assuncaosantos100@gmail.com>
2024-11-01 00:12:23 -0300

    Finaliza, por fim, a navbar com a responsividade adequada.

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,16 +1,7 @@
-/* Animação da abetura do buscar */
-const $liBuscar = document.querySelector("#navbar").children[1].children[1];
-const $inputSearchForm = $liBuscar.firstElementChild.lastElementChild;
-const $pBuscar = $liBuscar.lastElementChild;
-
 /* Animação ao clicar no ícone de menu */
 function movimentacaoMenuIcon(){
-    const $menuIcon = document.querySelector("#menuIcon");
+    const $menuIcon = document.querySelector("#navigationBar--menuIcon");
     $menuIcon.classList.toggle("muda");
-    const subSection = document.querySelector("#tabletAndMobileSubSection");
-    if($menuIcon.classList[0] == "muda"){
-        subSection.style.display = "flex";
-    }else{
-        subSection.style.display = "none";
-    }
+    const $subSection = document.querySelector("#tabletAndMobileSubSection");
+    $subSection.style.display = $menuIcon.classList[0] == "muda" ? "flex" : "none";
 }

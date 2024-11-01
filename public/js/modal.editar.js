725e4bc8fd358189bf354646060a4cb95de99200
Gabriel.cunha <greis.reis.2003@gmail.com>
2024-11-01 14:45:42 -0300

    Conclui o card e o javascript do modal editar

---
+++ public/js/modal.editar.js
@@ -0,0 +1,9 @@
+function editar(idModal, id2){
+    document.getElementById(idModal).style.display="block";
+    document.getElementById(id2).style.display="none"
+}
+
+function editar2(id3, id4){
+    document.getElementById(id3).style.display="none"
+    document.getElementById(id4).style.display="flex"
+}

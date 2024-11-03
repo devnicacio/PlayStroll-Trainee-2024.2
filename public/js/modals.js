1df2e0cf79ba3e3c04aff53f916114cb2a404ae5
Vitor Assuncao <vitor.assuncaosantos100@gmail.com>
2024-11-02 22:38:55 -0300

    Adiciona melhorias no HTML e CSS do read-modal-posts.

--- public/js/modals.js
+++ public/js/modals.js
@@ -2,10 +2,11 @@ const tela = document.querySelector('#tela');
 
 const abrirModal = idModal => {
     document.getElementById(idModal).style.display = "flex";
-    tela.style.display = 'block';
+    tela.style.display = "block";
+    tela.style.opacity = "0.5"
 }
 
 const fecharModal = idModal => {
     document.getElementById(idModal).style.display = "none";
-    tela.style.display = 'none';
+    tela.style.display = "none";
 }

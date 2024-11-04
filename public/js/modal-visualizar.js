4908e7b24d122af93bb824b8408cf19ca78cd9e1
Gabriel.cunha <greis.reis.2003@gmail.com>
2024-11-04 10:22:12 -0300

    Finaliza antes do merge

--- public/js/modal-visualizar.js
+++ public/js/modal-visualizar.js
@@ -1,11 +1,15 @@
+const tela = document.querySelector('#tela')
+
 function abrirModal(idModal, id2)
 {
     document.getElementById(idModal).style.display="flex";
     document.getElementById(id2).style.display="none"
+    tela.style.display="block"
 }
 
 function fecharModal(id3, id4)
 {
     document.getElementById(id3).style.display="none";
     document.getElementById(id4).style.display="flex";
+    tela.style.display="none"
 }
--- public/js/modal.editar.js
+++ public/js/modal.editar.js
@@ -1,9 +1,13 @@
+const tela = document.querySelector('#tela')
+
 function editar(idModal, id2){
     document.getElementById(idModal).style.display="block";
     document.getElementById(id2).style.display="none"
+    tela.style.display="block"
 }
 
 function editar2(id3, id4){
     document.getElementById(id3).style.display="none"
     document.getElementById(id4).style.display="flex"
+    tela.style.display="none"
 }

c4a81e8b6ca4938f563e60180baab966fab240f6
Gabriel.cunha <greis.reis.2003@gmail.com>
2024-11-08 08:55:33 -0300

    Opção de adicionar e remover imagem no modal de editar

---
+++ public/js/modal-editar.js
@@ -0,0 +1,52 @@
+const tela = document.querySelector('#tela')
+
+function editar(idModal, id2){
+    document.getElementById(idModal).style.display="flex";
+    document.getElementById(id2).style.display="none";
+    tela.style.display="block";
+}
+
+function editar2(id3, id4){
+    document.getElementById(id3).style.display="none";
+    document.getElementById(id4).style.display="flex";
+    tela.style.display="none";
+}
+
+function mostrarSenha(){
+    var inputPass = document.getElementById('senha');
+    var showPass = document.getElementById('mostrar');
+
+    if(inputPass.type==='password'){
+        inputPass.setAttribute('type', 'text');
+        showPass.classList.replace('bi-eye', 'bi-eye-slash');
+    }
+    else{
+        inputPass.setAttribute('type', 'password');
+        showPass.classList.replace('bi-eye-slash', 'bi-eye');
+    }
+}
+
+function trocarImagem(event){
+    const imagemIni = document.getElementById('imagemIni');
+    const arquivo = event.target.files[0];
+
+    if(arquivo){
+        imagemIni.src = URL.createObjectURL(arquivo);
+    }
+}
+
+document.getElementById("inputimage").addEventListener("change", trocarImagem);
+
+function removerImagem(id5, id6, id7){
+    const imagem = document.getElementById("imagemIni");
+    imagem.src="";
+    document.getElementById(id5).style.display = "none";
+    document.getElementById(id6).style.display = "none";
+    document.getElementById(id7).style.display = "flex";
+}
+
+function adicionaImagem(id8, id9, maisID){
+    document.getElementById(id8).style.display = "none";
+    document.getElementById(id9).style.display = "flex";
+    document.getElementById(maisID).style.display = "flex";
+}

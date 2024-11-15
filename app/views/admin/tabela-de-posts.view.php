2bd826bd4c294fc14768ed630ad033d84ce100ea
Diego Pereira Betti <diegopbetti.gmail.com>
2024-11-15 01:33:51 -0300

    Adiciona os componentes dos modais de criar e excluir posts na tabela e linka o CSS e JS tambem

--- app/views/admin/tabela-de-posts.view.php
+++ app/views/admin/tabela-de-posts.view.php
@@ -19,7 +19,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <!-- Personal CSS-->
     <link rel="stylesheet" href="/public/css/tabela-de-posts.css">
-
+	<link rel="stylesheet" type="text/css" href="/public/css/criar.css" />
+    <link rel="stylesheet" type="text/css" href="/public/css/excluir.css" />
     <!-- JS -->
     <script defer src="/public/js/modals.js"></script>
    
@@ -35,7 +36,7 @@
     <main class="container">
         <div class="cabecalho">
                 <h1 class="titulo">Tabela de Posts</h1>
-            <button class="more">+</button>
+            <button class="more" onclick="abrirModal('editar')">+</button>
         </div>
         <div class="table-container">
         <table class="tabela-posts">
@@ -58,7 +59,7 @@
                     <td>25/10/2024</td>
                     <td><button class="btn-acao btn-visualizar" onclick="abrirModal('modalPostVisualizar')"><i class="fa-regular fa-eye"></i></button></td>
                     <td><button class="btn-acao btn-editar" onclick="abrirModal('modalPostEditar')"><i class="fas fa-edit"></i></button></td>
-                    <td><button class="btn-acao btn-excluir"><i class="fas fa-trash"></i></button></td>
+                    <td><button class="btn-acao btn-excluir" onclick="abrirModal('editar')"><i class="fas fa-trash"></i></button></td>
                 </tr>
                 <tr>
                     <td>2</td>
@@ -115,7 +116,121 @@
             <button class="nav7">&gt;</button>
         </div>
     </div>
+
+    <!-- Parte da criação de posts     -->
+    <div class="modalc" id="editar">
+		<form action="#">
+			<div class="modal-container">
+				<div class="imagens">
+					<div class="capa">
+						<div class="container-image">
+							<label for="file">Foto modo paisagem</label>
+							<label class="custom-file-label" for="file-capa">Escolha uma imagem</label>
+						</div>
+						<div class="parte-capa">
+							<img id="file-name-capa" class="capa-preview" alt="Preview da Capa" />
+							<input type="file" class="image-capa" id="file-capa"
+							onchange="previewImage('file-capa', 'file-name-capa', 'capa')" />
+							<button id="btn-remover-imagem-capa" onclick="removerImagem('capa')">
+								X
+							</button>
+						</div>
+					</div>
+					<div class="retrato">
+						<div class="container-image">
+							<label for="file">Foto modo retrato</label>
+							<label class="custom-file-label" for="file-retrato">Escolha uma imagem</label>
+						</div>
+						<div class="parte-retrato">
+							<img id="file-name-retrato" class="retrato-preview" alt="Preview do Retrato" />
+							<input type="file" class="image-retrato" id="file-retrato"
+							onchange="previewImage('file-retrato', 'file-name-retrato', 'retrato')" />
+							<button id="btn-remover-imagem-retrato" onclick="removerImagem('retrato')">
+								X
+							</button>
+						</div>
+					</div>
+				</div>
+				<div>
+					<div class="placeholders">
+						<div class="parte-data">
+							<label for="date">Data</label>
+							<input type="date" id="data" class="data" >
+							<span id="erro-data" class="erro"></span>
+						</div>
+						<div class="second-line">
+							<div class="parte-titulo">
+								<label for="text">Título</label>
+								<input type="text" id="titulo" class="titulo" placeholder="Coloque seu título" />
+								<span id="erro-titulo" class="erro"></span>
+							</div>
+							<div class="parte-avaliacao">
+								<label for="number">Avaliação</label>
+								<input type="number"  id="avaliacao" class="avaliacao" step="0.5" min="0" max="5" placeholder="Nota de 0 a 5" />
+								<span id="erro-avaliacao" class="erro"></span>
+							</div>
+						</div>
+					</div>
+					<div class="diminuir-word">
+						<div id="summernote"></div>
+						<span id="erro-descricao" class="erro"></span>
+					</div>
+					<div class="modal-buttons">
+						<button id="btn-cancelar" class="button-cancelar" onclick="fecharModal('editar')">
+							Cancelar
+						</button>
+						<button id="btn-criar" class="button-postar">Criar</button>
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
+
+    <!-- Parte de excluir posts -->
+    <div class="modalc" id="editar">
+        <form action="#">
+          <div class="modal-container">
+            <img
+              src="/public/assets/deletar2.png"
+              alt=""
+              height="100px"
+              width="150px"
+            />
+            <h4>Tem certeza que deseja excluir?</h4>
+            <div class="modal-buttons">
+              <button class="button-cancelar" onclick="fecharModal('editar')">
+                Cancelar
+              </button>
+              <button class="button-excluir">Excluir</button>
+            </div>
+          </div>
+        </form>
+      </div>
+
+	<div class="tela" id="tela"></div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.9.0/lang/summernote-pt-BR.min.js"></script>
+	<script>
+		$("#summernote").summernote({
+			placeholder: "Crie a sua descrição",
+			tabsize: 2,
+			height: 120,
+			lang: "pt-BR",
+			toolbar: [
+				["style", ["style"]],
+				["font", ["bold", "underline"]],
+				["color", ["color"]],
+				["para", ["ul", "ol", "paragraph"]],
+				["table", ["table"]],
+				["insert", ["link", "picture"]],
+			],
+		});
+	</script>
 </body>
+    <script src="/public/js/criar.js"></script>
+    <script src="/public/js/excluir.js"></script>
+
+
     <script>
         $(".summernote").summernote('disable',{
 			tabsize: 2,

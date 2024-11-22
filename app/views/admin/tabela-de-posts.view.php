25e8dc6524c9610d696e7153bd1eec8eccd9fdb9
Diego Pereira Betti <diegopbetti.gmail.com>
2024-11-22 09:56:04 -0300

    Faz o modal excluir denovo e corrige a aparencia da paginacao

--- app/views/admin/tabela-de-posts.view.php
+++ app/views/admin/tabela-de-posts.view.php
@@ -72,11 +72,33 @@
                     <td><button class="btn-acao btn-excluir" onclick="abrirModal('excluir<?= $post->id ?>')"><i class="fas fa-trash"></i></button></td>
                 </tr>
                 <!-- Modais de leitura, alteração e exclusão--> 
+                <div class="modal-excluir" id="excluir<?= $post->id ?>">
+                    <form action="/deletar-post" method="post" >
+                        <input type="hidden" name="iddelete_post" value="<?= $post->id ?>">
+                        <input type="hidden" name="iddelete_capa" value="<?= $post->image_capa ?>">
+                        <input type="hidden" name="iddelete_retrato" value="<?= $post->image_retrato ?>">
+                            <div class="modal-container-excluir">
+                                <img
+                                src="/public/assets/deletar2.png"
+                                alt=""
+                                height="100px"
+                                width="150px"
+                                />
+                                <h4>Tem certeza que deseja excluir o post?</h4>
+                                <div class="modal-buttons-excluir">
+                                <button class="button-cancelar" type="button" onclick="fecharModal('excluir<?= $post->id ?>')">
+                                    Cancelar
+                                </button>
+                                <button class="button-excluir" type="submit">Excluir</button>
+                                </div>
+                            </div>
+                    </form>
+                </div>
                 <?php
                     require_once realpath(dirname(__FILE__)."/modais/posts/read_modal_posts.php");
                     require_once realpath(dirname(__FILE__)."/modais/posts/update_modal_posts.php");
-                    require_once realpath(dirname(__FILE__)."/modais/posts/delete_modal_posts.php");
                 ?>
+                
                 <?php endforeach?>
             </tbody>
             <div class="tela" id="tela"></div>

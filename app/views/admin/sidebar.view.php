8a7147019e327d4dfeef54cae1714d5152d4795e
Gabriel.cunha <greis.reis.2003@gmail.com>
2024-12-11 16:53:49 -0300

    Coloca as rotas na dashboard

--- app/views/admin/sidebar.view.php
+++ app/views/admin/sidebar.view.php
@@ -66,7 +66,8 @@
         </ul>
     </div>
     <div id="sideBar--logout">
-        <button id="logoutBtn">
+        <form action="/logout"  method="POST" >
+        <button id="logoutBtn" type="submit"  >
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="itensMenu" viewBox="0 0 16 16">
                 <path d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3zm1 13h8V2H4z"/>
                 <path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 0"/>
@@ -79,6 +80,7 @@
                 <path d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
             </svg>-->
         </button>
+        </form>
     </div>
 </nav>
 <script type="module" src="/public/js/admin/sidebar.js"></script>

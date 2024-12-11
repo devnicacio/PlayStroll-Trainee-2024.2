e3590b358e8b911bd48e293c22b3b68c4968cb29
Gabriel.cunha <greis.reis.2003@gmail.com>
2024-12-10 23:23:02 -0300

    Adiciona o footer à landing page

--- app/views/site/landing-page.view.php
+++ app/views/site/landing-page.view.php
@@ -80,6 +80,8 @@
             </div>
     </main>
 
+    <?php include 'footer.view.php'; ?>
+
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <script src="/public/js/landing-page.js"></script>

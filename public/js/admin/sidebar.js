8325ecb052e1dfa4daae2026ffcc6a33c774b9ed
Gabriel.cunha <greis.reis.2003@gmail.com>
2024-12-18 17:29:55 -0300

    Faz modificações na sidebar e corrige a altura do placeholder do input da navbar

--- public/js/admin/sidebar.js
+++ public/js/admin/sidebar.js
@@ -1,8 +1,11 @@
 // Animação de abertura da caption do menu
 const $sideBar = document.querySelector("#sideBar");
+
+if(window.innerWidth > 768){
 $sideBar.addEventListener('mouseover', ()=>{
     $sideBar.classList.add("open-sideBar");
 })
 $sideBar.addEventListener('mouseout', ()=>{
     $sideBar.classList.remove("open-sideBar");
 })
+}

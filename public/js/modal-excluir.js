7b56e41754ecde1422ed4bdfc15f941f5e1758a6
JoaoMendes <jopascode@gmail.com>
2024-12-18 18:44:22 -0300

    Coloca o fundo escuro no modal de excluir usuario

--- public/js/modal-excluir.js
+++ public/js/modal-excluir.js
@@ -1,11 +1,12 @@
 // Função para abrir o modal de exclusão de usuário
 function abrirModalExcluirUsuario(modalId) {
     const modal = document.getElementById(modalId);
-    modal.style.display = 'flex';
+    modal.style.display = 'flex'; // Exibe o modal
 
     const tela = document.querySelector('#tela');
-    if (tela) tela.style.display = 'block';
+    if (tela) tela.style.display = 'block'; // Exibe o fundo escurecido
 }
+
 
 // Função para fechar o modal
 function fecharModalExcluirUsuario(modalId) {
